3f16a47556319fb267fc0af3c23b5a0871bc6036
emircanbakar <emirbakar952@gmail.com>
2024-08-03 19:27:41 +0300

    mongoDb bağlantısı

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import client from "@/lib/mongodb";
 
 export const authOptions = {
-
+  adapter: MongoDBAdapter(client),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
