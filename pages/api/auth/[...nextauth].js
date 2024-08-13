d3524c224a31bfad69fe3fd6e2da759d0a6544f9
emircanbakar <emirbakar952@gmail.com>
2024-08-13 04:49:37 +0300

    auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,26 +1,30 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "@/lib/mongodb";
-
-
 
 export default NextAuth({
   adapter: MongoDBAdapter(client),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code'
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async signIn({ account, profile }) {
-      if (account.provider === 'google') {
-        return profile.email_verified && profile.email.endsWith('@gmail.com')
+      if (account.provider === "google") {
+        return profile.email_verified && profile.email.endsWith("@gmail.com");
       }
-      return true
+      return true;
     },
   },
-})
+});
