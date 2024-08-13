c59da0360297582852b9209665f80f6ca3f2f9ea
emircanbakar <emirbakar952@gmail.com>
2024-08-13 04:35:39 +0300

    trying to fix next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,15 +3,24 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "@/lib/mongodb";
 
-export const authOptions = {
+
+
+export default NextAuth({
   adapter: MongoDBAdapter(client),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code'
+    }),
   ],
-  secret: process.env.NEXTAUTH_SECRET
-}
-
-export default NextAuth(authOptions)
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account.provider === 'google') {
+        return profile.email_verified && profile.email.endsWith('@gmail.com')
+      }
+      return true
+    },
+  },
+})
